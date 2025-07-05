1081558eb89f0e25a3d2bc8e6edaafb12e0f4740
akshayparate92 <akshayparate92@gmail.com>
2025-07-05 17:16:15 +0530

    analytics add

--- pages/news.js
+++ pages/news.js
@@ -1,8 +1,13 @@
+import styles from "@/styles/Home.module.css";
+import { Analytics } from "@vercel/analytics/next"
 export default function News() {
   return (
+    <main className={styles.main}>
+        <Analytics />
     <div>
       <h1>News</h1>
       <p>Latest news updates will be displayed here.</p>
     </div>
+    </main>
   );
 }
